70861c33f9fdbca860352b469344c2ea2c6ce7b9
Fann Wu <fann.wu@pathfinders.studio>
2023-02-22 17:38:37 +0800

    Fixed cros error.

--- cryptoKeno/src/App.vue
+++ cryptoKeno/src/App.vue
@@ -1,67 +1,103 @@
 <template>
   <div>
     <table>
-      <tr v-for="i in 4" :key="i">
-        <td v-for="j in 10" :key="10*(i-1)+j">
-          <button :class="{ selected: isSelected(10*(i-1)+j) }" @click="selectButton(10*(i-1)+j)">{{ 10*(i-1)+j }}</button>
+      <tr v-for="(row, rowIndex) in buttonRows" :key="rowIndex">
+        <td v-for="(button, buttonIndex) in row" :key="buttonIndex">
+          <button :class="{ selected: selectedButtons.includes(button) }" @click="toggleSelection(button)">
+            {{ button }}
+          </button>
         </td>
       </tr>
     </table>
-    <button @click="submitSelection" :disabled="selectedButtons.length === 0">Submit Selection</button>
-    <div v-if="win">
-      <h2>You win {{ win }}</h2>
-    </div>
+    <form @submit.prevent="submitBet">
+      <label for="amount">Bet Amount:</label>
+      <input type="number" id="amount" v-model="betAmount" step="0.01" min="0">
+      <button type="submit" :disabled="selectedButtons.length === 0 || selectedButtons.length > 10">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+// import { looseIndexOf } from '@vue/shared';
+
 export default {
   data() {
     return {
-      buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+      buttonRows: [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+        [21, 22, 23, 24],
+        [25, 26, 27, 28],
+        [29, 30, 31, 32],
+        [33, 34, 35, 36],
+        [37, 38, 39, 40],
+      ],
       selectedButtons: [],
-      win: null
-    }
+      betAmount: 0,
+    };
   },
   methods: {
-    selectButton(value) {
-      if (this.selectedButtons.includes(value)) {
-        this.selectedButtons.splice(this.selectedButtons.indexOf(value), 1);
-      } else if (this.selectedButtons.length < 10) {
-        this.selectedButtons.push(value);
+    toggleSelection(button) {
+      if (this.selectedButtons.includes(button)) {
+        this.selectedButtons.splice(this.selectedButtons.indexOf(button), 1);
+      } else {
+        this.selectedButtons.push(button);
       }
     },
-    isSelected(value) {
-      return this.selectedButtons.includes(value);
-    },
-    submitSelection() {
-      if (this.selectedButtons.length > 0) {
-        const data = JSON.stringify({ selectedButtons: this.selectedButtons });
-        fetch('http://127.0.0.1:8080/crypto', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: data
-        })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error('Network response was not ok.');
-        })
-        .then(json => {
-          console.log('Response:', json);
-          this.win = json.win;
+    submitBet() {
+      const data = {
+        // selectedFields: this.selectedButtons,
+        // betAmount: this.betAmount,
+        selectedFields: [1, 2, 3],
+        betAmount: 0.05
+      };
+      fetch("http://localhost:5566/cryptokeon", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", // 加入這個屬性
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          alert(`You won ${data.win}!`);
           this.selectedButtons = [];
+          this.betAmount = 0;
         })
-        .catch(error => console.error('Error:', error));
-      }
-    }
-  }
-}
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("Error occurred while processing your bet!");
+        });
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+td {
+  padding: 0;
+}
+
+button {
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+  border: none;
+  background-color: #f7f7f7;
+  cursor: pointer;
+}
+
 .selected {
-  background-color: yellow;
+  background-color: #1abc9c;
+  color: white;
 }
 </style>
