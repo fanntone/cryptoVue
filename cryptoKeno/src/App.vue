7c1309b27b03c7b1b676affae46564dd6a1f6d80
Fann Wu <fann.wu@pathfinders.studio>
2023-02-24 14:29:24 +0800

    Add bet history table.

--- cryptoKeno/src/App.vue
+++ cryptoKeno/src/App.vue
@@ -11,32 +11,43 @@
     </table>
     <form @submit.prevent="submitBet">
       <label for="amount">Bet Amount:</label>
-      <input type="number" id="amount" v-model="betAmount" step="0.01" min="0">
+      <input type="number" id="amount" v-model="betAmount" step="0.000001" min="0">
       <button type="submit" :disabled="selectedButtons.length === 0 || selectedButtons.length > 10">Submit</button>
     </form>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Payout</th>
+          <th>WinFields</th>
+          <th>Profit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in betResults" :key="rowIndex">
+          <td>{{ row.Payout }}</td>
+          <td>{{ row.WinFields }}</td>
+          <td>{{ row.Profit.toFixed(8) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
 <script>
-// import { looseIndexOf } from '@vue/shared';
-
 export default {
   data() {
     return {
       buttonRows: [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-        [17, 18, 19, 20],
-        [21, 22, 23, 24],
-        [25, 26, 27, 28],
-        [29, 30, 31, 32],
-        [33, 34, 35, 36],
-        [37, 38, 39, 40],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+        [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
       ],
       selectedButtons: [],
       betAmount: 0,
+      betResults: [],
     };
   },
   methods: {
@@ -49,10 +60,8 @@ export default {
     },
     submitBet() {
       const data = {
-        // selectedFields: this.selectedButtons,
-        // betAmount: this.betAmount,
-        selectedFields: [1, 2, 3],
-        betAmount: 0.05
+        selectedField: this.selectedButtons,
+        betAmount: this.betAmount,
       };
       fetch("http://localhost:5566/cryptokeon", {
         method: "POST",
@@ -60,14 +69,15 @@ export default {
         headers: {
           "Content-Type": "text/plain; charset=utf-8",
         },
-        credentials: "include", // 加入這個屬性
+        credentials: "include",
       })
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          alert(`You won ${data.win}!`);
+          alert(`You won ${data.Profit.toFixed(8)}!`);
           this.selectedButtons = [];
           this.betAmount = 0;
+          this.betResults.push(data);
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -78,26 +88,40 @@ export default {
 };
 </script>
 
+
 <style scoped>
-table {
-  border-collapse: collapse;
-}
+  table {
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
 
-td {
-  padding: 0;
-}
+  td,
+  th {
+    border: 1px solid #ccc;
+    padding: 8px;
+  }
 
-button {
-  width: 100%;
-  height: 100%;
-  font-size: 14px;
-  border: none;
-  background-color: #f7f7f7;
-  cursor: pointer;
-}
+  button {
+    width: 100%;
+    height: 100%;
+    font-size: 25px;
+    border: none;
+    background-color: #f7f7f7;
+    cursor: pointer;
+  }
 
-.selected {
-  background-color: #1abc9c;
-  color: white;
-}
+  .selected {
+    background-color: #1abc9c;
+    color: white;
+  }
+
+  .bet-result {
+    margin-top: 20px;
+  }
+
+  .bet-result th,
+  .bet-result td {
+    padding: 5px 10px;
+    text-align: center;
+  }
 </style>
